5f7a53b4588bcc03e1f40bf5a4d268249456d7c2
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2025-01-12 00:57:23 +0900

    first commit

---
+++ src/main/java/inha/jungseokapp/JungseokAppApplication.java
@@ -0,0 +1,13 @@
+package inha.jungseokapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungseokAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungseokAppApplication.class, args);
+	}
+
+}
